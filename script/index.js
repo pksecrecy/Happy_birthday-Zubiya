c5e4bf43be8b1db658355a54dcff39c6e07b08c2
Parjanya Kumar Arya <Parjanya789@gmail.com>
2022-08-17 17:37:49 +0530

    2022

--- script/index.js
+++ script/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-    birthdate: 'August 25, 2021',
+    birthdate: 'August 25, 2022 ',
     name: 'ZUBIYA'
 };
 
